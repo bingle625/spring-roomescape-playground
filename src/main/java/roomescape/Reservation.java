d7133d8985ee890037064af425d253a9e477d270
bingle625 <fhqhxm8899@gmail.com>
2024-11-01 22:38:19 +0900

    add: repository

--- src/main/java/roomescape/Reservation.java
+++ src/main/java/roomescape/Reservation.java
@@ -1,8 +1,20 @@
 package roomescape;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Reservation {
 
+  protected Reservation() {
+  }
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
+
   private String name;
   private String date;
   private String time;
