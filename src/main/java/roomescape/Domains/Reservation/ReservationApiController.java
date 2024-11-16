d08d45581a93f0c141ee65ba1d09f04bd6c29504
bingle625 <fhqhxm8899@gmail.com>
2024-11-16 20:31:49 +0900

    refactor: ReservationService, TimeService 분리

--- src/main/java/roomescape/Domains/Reservation/ReservationApiController.java
+++ src/main/java/roomescape/Domains/Reservation/ReservationApiController.java
@@ -11,21 +11,22 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import roomescape.Domains.Time.Time;
-import roomescape.Domains.Time.TimeRepository;
 import roomescape.exceptions.BadRequestException;
 
 @RestController
 public class ReservationApiController {
 
-  @Autowired
-  private ReservationRepository reservationRepository;
-  @Autowired
-  private TimeRepository timeRepository;
+  private final ReservationService reservationService;
+
+  public ReservationApiController(final ReservationService reservationService) {
+    this.reservationService = reservationService;
+  }
+
 
   @GetMapping("/reservations")
   public ResponseEntity<List<Reservation>> getReservations() {
-    List<Reservation> reservations = this.reservationRepository.findAll();
+    List<Reservation> reservations = reservationService.list();
+
     return ResponseEntity.ok().body(reservations);
   }
 
@@ -36,20 +37,15 @@ public class ReservationApiController {
         Objects.isNull(requestDto.name()) || requestDto.name().isEmpty()) {
       throw new BadRequestException("올바르지 않은 입력입니다.");
     }
-    Time time = timeRepository.findFirstByTime(requestDto.time())
-        .orElseThrow(() -> new BadRequestException("존재하지 않는 시간입니다."));
-    Reservation newReservation = reservationRepository.save(
-        new Reservation(requestDto.name(), requestDto.date(), time));
+    Reservation reservation = reservationService.create(requestDto);
 
-    return ResponseEntity.created(URI.create("/reservations/" + newReservation.getId()))
-        .body(newReservation);
+    return ResponseEntity.created(URI.create("/reservations/" + reservation.getId()))
+        .body(reservation);
   }
 
   @DeleteMapping("/reservations/{id}")
   public ResponseEntity<Void> delete(@PathVariable Long id) {
-    Reservation reservation = this.reservationRepository.findById(id)
-        .orElseThrow(() -> new BadRequestException("존재하지 않는 예약입니다."));
-    this.reservationRepository.delete(reservation);
+    reservationService.deleteByIdOrElseThrowException(id);
 
     return ResponseEntity.noContent().build();
   }
--- src/main/java/roomescape/Domains/Time/TimeApiController.java
+++ src/main/java/roomescape/Domains/Time/TimeApiController.java
@@ -2,7 +2,6 @@ package roomescape.Domains.Time;
 
 import java.net.URI;
 import java.util.List;
-import java.util.Map;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,43 +9,33 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import roomescape.Domains.Reservation.Reservation;
-import roomescape.exceptions.BadRequestException;
 
 @RestController
 public class TimeApiController {
 
-  private final TimeRepository timeRepository;
+  private final TimeService timeService;
 
-  public TimeApiController(TimeRepository timeRepository) {
-    this.timeRepository = timeRepository;
+  public TimeApiController(TimeService timeService) {
+    this.timeService = timeService;
   }
 
   @GetMapping("/times")
   public ResponseEntity<List<Time>> index() {
-    List<Time> times = timeRepository.findAll();
+    List<Time> times = timeService.list();
 
     return ResponseEntity.ok().body(times);
   }
 
   @PostMapping("/times")
-  public ResponseEntity<Time> store(@RequestBody Object request) {
-    Time newTime = new Time();
-    if (request instanceof Map) {
-      Map<String, Object> map = (Map<String, Object>) request;
-      newTime = new Time((String) map.get("time"));
-      timeRepository.save(newTime);
-    } else {
-      throw new BadRequestException("ㅈ");
-    }
-    return ResponseEntity.created(URI.create("/times/1")).body(newTime);
+  public ResponseEntity<Time> store(@RequestBody TimeStoreRequestDto requestDto) {
+    Time time = timeService.create(requestDto);
+
+    return ResponseEntity.created(URI.create("/times/1")).body(time);
   }
 
   @DeleteMapping("/times/{id}")
   public ResponseEntity<Void> delete(@PathVariable Long id) {
-    Time time = timeRepository.findById(id)
-        .orElseThrow(() -> new BadRequestException("존재하지 않는 시간입니다."));
-    timeRepository.delete(time);
+    timeService.deleteByIdOrElseThrowException(id);
 
     return ResponseEntity.noContent().build();
   }
