14bc0079c008480786ed807783a2550df1e3dcb1
bingle625 <fhqhxm8899@gmail.com>
2024-12-28 02:08:58 +0900

    feat: jpaRepository를 통해 로그인 정보 가져오도록 수정

--- src/main/java/roomescape/Domains/Auth/LoginController.java
+++ src/main/java/roomescape/Domains/Auth/LoginController.java
@@ -15,22 +15,29 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LoginController {
+
   final private String secretKey = "Yn2kjibddFAWtnPJ2AFlL8WXmohJMCvigQggaEypa5E=";
+  private final UserRepository userRepository;
+
+  public LoginController(final UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
   @PostMapping("/login")
   public final ResponseEntity<String> login(@RequestBody Map<String, String> credentials) {
     String password = credentials.get("password");
     String email = credentials.get("email");
-    Claims claims = Jwts.claims().setSubject(email);
+    String name = "어드민";
+
+    this.userRepository.save(new User(name, email, password));
+
+    Claims claims = Jwts.claims().setSubject(name);
 
     Date now = new Date();
     Date expiryDate = new Date(now.getTime() + 1000L * 60 * 60); // 토큰 만료 시간: 1시간 후
-//
+
     String accessToken = Jwts.builder().setClaims(claims).setIssuedAt(now).setExpiration(expiryDate)
         .signWith(SignatureAlgorithm.HS256, secretKey).compact();
-
-//    Cookie cookie = new Cookie("token", accessToken);
-//    cookie.setHttpOnly(true);
-//    cookie.setPath("/");
 
     return ResponseEntity.ok().header("Set-Cookie", "token=" + accessToken + ";").build();
   }
@@ -38,18 +45,18 @@ public class LoginController {
   @GetMapping("/login/check")
   public final ResponseEntity<Map<String, String>> loginCheck(HttpServletRequest request) {
     Cookie[] cookies = request.getCookies();
-//    String token = getToken(cookies);
-////    Long memberId = Long.valueOf(Jwts.parser()
-////        .setSigningKey(secretKey)
-////        .parseClaimsJws(token)
-////        .getBody().getSubject());
-    return ResponseEntity.ok().body(Map.of("name", "어드민"));
+    String token = getToken(cookies);
+    String memberName = String.valueOf(Jwts.parser()
+        .setSigningKey(secretKey)
+        .parseClaimsJws(token)
+        .getBody().getSubject());
+    return ResponseEntity.ok().body(Map.of("name", memberName));
   }
 
   private static String getToken(final Cookie[] cookies) {
     for (Cookie cookie : cookies) {
       if (cookie.getName().equals("token")) {
-       return cookie.getValue();
+        return cookie.getValue();
       }
     }
     return "";
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS reservation;
 DROP TABLE IF EXISTS time;
+DROP TABLE IF EXISTS customer;
 
 CREATE TABLE time
 (
@@ -16,6 +17,15 @@ CREATE TABLE reservation
     time_id BIGINT,
     PRIMARY KEY (id),
     FOREIGN KEY (time_id) REFERENCES time(id)
+);
+
+CREATE TABLE customer
+(
+    id       BIGINT       NOT NULL AUTO_INCREMENT,
+    name     VARCHAR(255) NOT NULL,
+    email    VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
 );
 
 
